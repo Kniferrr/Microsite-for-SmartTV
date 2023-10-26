02716cab48a2d03f7d8c9c3c85a4dc521200a32f
Knifer <yeded73422@invodua.com>
2023-10-26 13:48:48 +0300

    refactoring keyboardHelpers

--- src/Helpers/keyboardHelpers.ts
+++ src/Helpers/keyboardHelpers.ts
@@ -32,50 +32,20 @@ export const handleKeyPress = (
       }
       break;
     case "Enter":
-      if (buttons[currentSelection] == "OkPromoPage") {
-        navigate("/number-input");
-      } else if (buttons[currentSelection] == "Exit") {
-        navigate("/");
-      } else if (
-        parseInt(buttons[currentSelection]) >= 1 &&
-        parseInt(buttons[currentSelection])
-      ) {
-        dispatch(setNumberValue(buttons[currentSelection]));
-      } else if (buttons[currentSelection] === "0") {
-        dispatch(setNumberValue("0"));
-      } else if (buttons[currentSelection] === "NubmerInputClear") {
-        dispatch(delLastNumberValue());
-      } else if (buttons[currentSelection] === "NubmerInputCheckBox") {
-        dispatch(setReturnCheckBox());
-      } else if (buttons[currentSelection] === "OkNubmerInputPage") {
-        validateNumber(numberValue, navigate, checkBoxState, dispatch);
-      }
+      handleEnter(
+        currentSelection,
+        buttons,
+        navigate,
+        dispatch,
+        numberValue,
+        checkBoxState
+      );
       break;
     case "ArrowUp":
-      if (currentSelection >= 10 && currentSelection <= 12) {
-        dispatch(setCurrentSelection(currentSelection - 2));
-      } else if (currentSelection >= 13) {
-        dispatch(setCurrentSelection(currentSelection - 1));
-      } else if (currentSelection === 0) {
-        dispatch(setCurrentSelection(1));
-      } else if (currentSelection > 3) {
-        dispatch(setCurrentSelection(currentSelection - 3));
-      }
+      dispatch(setCurrentSelection(handleArrowUp(currentSelection)));
       break;
     case "ArrowDown":
-      if (
-        currentSelection > 0 &&
-        currentSelection < 8 &&
-        buttons.length !== 1
-      ) {
-        dispatch(setCurrentSelection(currentSelection + 3));
-      } else if (currentSelection >= 8 && currentSelection <= 10) {
-        dispatch(setCurrentSelection(currentSelection + 2));
-      } else if (currentSelection >= 11) {
-        dispatch(setCurrentSelection(currentSelection + 1));
-      } else if (currentSelection === 0) {
-        dispatch(setCurrentSelection(1));
-      }
+      dispatch(setCurrentSelection(handleArrowDown(currentSelection, buttons)));
       break;
     case "Escape":
       navigate("/");
@@ -90,3 +60,61 @@ export const handleKeyPress = (
       }
   }
 };
+
+function handleArrowUp(currentSelection: number): number {
+  if (currentSelection >= 13) {
+    return currentSelection - 1;
+  } else if (currentSelection >= 10) {
+    return currentSelection - 2;
+  } else if (currentSelection > 3) {
+    return currentSelection - 3;
+  } else if (currentSelection === 0) {
+    return 1;
+  }
+  return currentSelection;
+}
+
+function handleArrowDown(currentSelection: number, buttons: string[]): number {
+  if (currentSelection >= 11) {
+    return currentSelection + 1;
+  } else if (currentSelection >= 8) {
+    return currentSelection + 2;
+  } else if (
+    currentSelection > 0 &&
+    currentSelection < 8 &&
+    buttons.length !== 1
+  ) {
+    return currentSelection + 3;
+  } else if (currentSelection === 0) {
+    return 1;
+  }
+  return currentSelection;
+}
+
+function handleEnter(
+  currentSelection: number,
+  buttons: Array<string>,
+  navigate: (path: string) => void,
+  dispatch: (action: MyAction) => void,
+  numberValue: string = "",
+  checkBoxState: boolean = false
+) {
+  if (buttons[currentSelection] == "OkPromoPage") {
+    navigate("/number-input");
+  } else if (buttons[currentSelection] == "Exit") {
+    navigate("/");
+  } else if (
+    parseInt(buttons[currentSelection]) >= 1 &&
+    parseInt(buttons[currentSelection])
+  ) {
+    dispatch(setNumberValue(buttons[currentSelection]));
+  } else if (buttons[currentSelection] === "0") {
+    dispatch(setNumberValue("0"));
+  } else if (buttons[currentSelection] === "NubmerInputClear") {
+    dispatch(delLastNumberValue());
+  } else if (buttons[currentSelection] === "NubmerInputCheckBox") {
+    dispatch(setReturnCheckBox());
+  } else if (buttons[currentSelection] === "OkNubmerInputPage") {
+    validateNumber(numberValue, navigate, checkBoxState, dispatch);
+  }
+}
